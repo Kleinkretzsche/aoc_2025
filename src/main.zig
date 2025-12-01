40783dad69ee9e725fd88c22e831b4dd8b9d72de
Ruuis <luiskret@gmail.com>
2025-12-01 09:13:23 +0100

    finished logic. parsing and reading input is veeeery difficult

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
 
+fn par1(directions: std.ArrayList(bool), ammounts: std.ArrayList(i64)) u64 {
+    var zeros: u64 = 0;
+    var position: i64 = 50;
+    for (directions.itmes, ammounts.items) |dir, ammount| {
+        if (dir) {
+            position += ammount;
+        } else {
+            position -= ammount;
+        }
+        if (position % 100 == 0) {
+            zeros += 1;
+        }
+    }
+    return zeros;
+}
+
 pub fn main() !void {
-    std.debug.print("Hello world\n", .{});
+    return error.ImplementParsing;
 }
